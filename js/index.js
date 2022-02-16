5aff90c30789aec2aa7553d0306b477050cdd94d
HarshPipaliya <pipaliyaharsh0@gmail.com>
2022-02-16 17:57:07 +0530

    Initial Comment

---
+++ js/index.js
@@ -0,0 +1,28 @@
+console.log('Welcome to project background color changer');
+
+let container = document.querySelector('.container');
+let btn = document.getElementById('btn');
+
+
+// Change Backgound by clicking button
+// btn.addEventListener('click',()=>{
+//     console.log('This is a button');
+// let red = Math.floor(Math.random() * (200-1) + 1);
+// let green = Math.floor(Math.random() * (200-1) + 1);
+// let blue = Math.floor(Math.random() * (200-1) + 1);
+// console.log(red);
+// console.log(green);
+// console.log(blue);
+// container.style.backgroundColor = `rgb(${red},${green},${blue})`; 
+// });
+
+// Change Backgound automatically
+setInterval(() => {
+    let red = Math.floor(Math.random() * (200 - 1) + 1);
+    let green = Math.floor(Math.random() * (200 - 1) + 1);
+    let blue = Math.floor(Math.random() * (200 - 1) + 1);
+    // console.log(red);
+    // console.log(green);
+    // console.log(blue);
+    container.style.backgroundColor = `rgb(${red},${green},${blue})`;
+}, 1000);
